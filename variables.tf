e171261b86368514efae9988df2fa59eeed099c4
Shailesh <shailesh.radkar84@gmail.com>
2023-10-28 12:01:39 +0530

    terraform Ec2 instance creation code

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  description = "this region"
+}
+variable "ami" {
+  description = "AMI id"
+}
+variable "instance_type" {
+  description = "instance type"
+}
+variable "name" {
+    description = "tag name" 
+}
+variable "volume_size" {
+  description = "add the memory size"
+}
